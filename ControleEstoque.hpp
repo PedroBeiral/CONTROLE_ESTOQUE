7b44390dd34e2da005e1635884ac519e5c61f39e
vidantas10 <136518471+vidantas10@users.noreply.github.com>
2024-06-23 22:30:19 -0300

    Update ControleEstoque.hpp

--- ControleEstoque.hpp
+++ ControleEstoque.hpp
@@ -6,17 +6,29 @@
 #include "BancoDados.hpp"
 #include "Produto.hpp"
 
+// Classe ControleEstoque gerencia as operações de controle de estoque
 class ControleEstoque {
 public:
+    // Construtor que inicializa o controle de estoque com o nome do arquivo de banco de dados
     ControleEstoque(const std::string& nomeArquivo);
+
+    // Adiciona um produto ao estoque
     void adicionaProduto(const Produto& produto);
+
+    // Remove um produto do estoque pelo ID
     void removeProduto(int id);
+
+    // Atualiza um produto no estoque pelo ID e atributo
     void atualizaProduto(int id, const std::string& atributo, const std::string& novoValor);
+
+    // Exibe as informações de um produto pelo ID
     void exibeProduto(int id);
-    std::vector<Produto> buscarTodosOsProdutos() const; // Adicionado método para buscar todos os produtos
+
+    // Busca todos os produtos no banco de dados
+    std::vector<Produto> buscarTodosOsProdutos() const;
 
 private:
-    BancoDados banco;
+    BancoDados banco; // Instância de BancoDados para gerenciar as operações de banco de dados
 };
 
 #endif // CONTROLEESTOQUE_HPP
