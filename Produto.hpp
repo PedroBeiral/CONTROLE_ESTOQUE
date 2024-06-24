6e7050fcafac053316ac12fb4f20904d66441fe9
arthur-magno <arthurmscordeiro@outlook.com>
2024-06-23 22:31:56 -0300

    Update Produto.hpp

--- Produto.hpp
+++ Produto.hpp
@@ -3,10 +3,13 @@
 
 #include <string>
 
+// Classe Produto representa um produto genérico no estoque
 class Produto {
 public:
+    // Construtor que inicializa um produto com ID, nome, marca, preço, fornecedor e setor de armazenamento
     Produto(int id, const std::string& nome, const std::string& marca, double preco, const std::string& fornecedor, char setorArmazenamento);
 
+    // Métodos de acesso (getters)
     int acessoID() const;
     std::string acessoNOME() const;
     std::string acessoMARCA() const;
@@ -14,6 +17,7 @@ public:
     std::string acessoFORNECEDOR() const;
     char acessoSETOR() const;
 
+    // Métodos de modificação (setters)
     void setNome(const std::string& nome);
     void setMarca(const std::string& marca);
     void setPreco(double preco);
@@ -21,12 +25,12 @@ public:
     void setSetor(char setorArmazenamento);
 
 private:
-    int _id;
-    std::string _nome;
-    std::string _marca;
-    double _preco;
-    std::string _fornecedor;
-    char _setorArmazenamento;
+    int _id;  // Identificador do produto
+    std::string _nome;  // Nome do produto
+    std::string _marca;  // Marca do produto
+    double _preco;  // Preço do produto
+    std::string _fornecedor;  // Fornecedor do produto
+    char _setorArmazenamento;  // Setor de armazenamento do produto
 };
 
 #endif // PRODUTO_HPP
