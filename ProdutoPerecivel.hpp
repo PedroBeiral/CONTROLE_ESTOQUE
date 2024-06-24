7c7b8e622461763d9dad7a87d4934e50c679bd34
arthur-magno <arthurmscordeiro@outlook.com>
2024-06-23 22:32:35 -0300

    Update ProdutoPerecivel.hpp

--- ProdutoPerecivel.hpp
+++ ProdutoPerecivel.hpp
@@ -4,15 +4,20 @@
 #include "Produto.hpp"
 #include "Data.hpp"
 
+// Classe ProdutoPerecivel representa um produto perecível no estoque
 class ProdutoPerecivel : public Produto {
 public:
+    // Construtor que inicializa um produto perecível com data de vencimento
     ProdutoPerecivel(int id, const std::string& nome, const std::string& marca, double preco, const std::string& fornecedor, char setorArmazenamento, const Data& dataVencimento);
 
+    // Retorna a data de vencimento do produto perecível
     Data getDataVencimento() const;
+
+    // Verifica se o produto está próximo do vencimento
     bool estaProximoDoVencimento() const;
 
 private:
-    Data dataVencimento;
+    Data dataVencimento; // Data de vencimento do produto perecível
 };
 
 #endif // PRODUTOPERECIVEL_HPP
