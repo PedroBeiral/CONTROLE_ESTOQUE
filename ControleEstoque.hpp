bc603d1bb0e900c592bd5da7d8c0160b1652e707
arthur-magno <arthurmscordeiro@outlook.com>
2024-06-23 21:53:25 -0300

    Update ControleEstoque.hpp

--- ControleEstoque.hpp
+++ ControleEstoque.hpp
@@ -2,6 +2,7 @@
 #define CONTROLEESTOQUE_HPP
 
 #include <string>
+#include <vector>
 #include "BancoDados.hpp"
 #include "Produto.hpp"
 
@@ -12,6 +13,7 @@ public:
     void removeProduto(int id);
     void atualizaProduto(int id, const std::string& atributo, const std::string& novoValor);
     void exibeProduto(int id);
+    std::vector<Produto> buscarTodosOsProdutos() const; // Adicionado método para buscar todos os produtos
 
 private:
     BancoDados banco;
