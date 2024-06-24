00d41be5b9992f15dc0653561766f9fa02433e86
arthur-magno <arthurmscordeiro@outlook.com>
2024-06-23 21:38:56 -0300

    Update ControleEstoque.hpp

--- ControleEstoque.hpp
+++ ControleEstoque.hpp
@@ -1,10 +1,20 @@
 #ifndef CONTROLEESTOQUE_HPP
 #define CONTROLEESTOQUE_HPP
 
-#include <iostream>
-
-#include "Data.hpp"
+#include <string>
+#include "BancoDados.hpp"
 #include "Produto.hpp"
 
+class ControleEstoque {
+public:
+    ControleEstoque(const std::string& nomeArquivo);
+    void adicionaProduto(const Produto& produto);
+    void removeProduto(int id);
+    void atualizaProduto(int id, const std::string& atributo, const std::string& novoValor);
+    void exibeProduto(int id);
 
-#endif
+private:
+    BancoDados banco;
+};
+
+#endif // CONTROLEESTOQUE_HPP
